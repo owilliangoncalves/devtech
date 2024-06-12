25a975a570c6675a7580f9124c86c08edf3160ad
Willian Goncalves <williangoncalves74@gmail.com>
2024-06-12 17:27:34 -0300

    feat novas rotas

---
+++ src/app/manifesto/page.tsx
@@ -0,0 +1,7 @@
+export default function Manifesto() {
+  return (
+    <div>
+      <h1>Página sobre o Manifesto</h1>
+    </div>
+  );
+}
---
+++ src/app/nao-somos/page.tsx
@@ -0,0 +1,7 @@
+export default function NaoSomos() {
+  return (
+    <div>
+      <h1>Página sobre o que não somos</h1>
+    </div>
+  );
+}
