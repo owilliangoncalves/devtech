7f45b4dbc5d0462e3df5092e7e40a5f652a553ea
Willian Goncalves <williangoncalves74@gmail.com>
2024-06-14 11:42:18 -0300

    add: Texto de manifesto DEV Experts

--- src/app/pages/eng-software/layout.tsx
+++ src/app/pages/eng-software/layout.tsx
@@ -5,7 +5,6 @@ export default function EngSoftwareLayout() {
   return (
     <div>
       <NavBar />
-
       <EngSoftware />
     </div>
   );
--- src/app/pages/manifesto/page.tsx
+++ src/app/pages/manifesto/page.tsx
@@ -1,7 +1,59 @@
 export default function Manifesto() {
   return (
     <div>
-      <h1>Página sobre o Manifesto</h1>
+      <h1>Manifesto DEVS Experts</h1>
+      <p>
+        Em um mundo cada vez mais digital, onde a tecnologia desempenha um papel
+        central em nossas vidas, acreditamos que o conhecimento em programação e
+        áreas afins deve ser acessível a todos. Não somos uma escola de
+        programação e não estamos aqui para vender cursos, queremos transformar
+        a forma como o conhecimento é transmitido e recebido, tornando-o mais
+        inclusivo, acessível e livre.
+      </p>
+      <p>
+        Por isso, nos propomos a abordar os mesmos tópicos que você deverá
+        encontrar na faculdade de tecnologia, mas de uma maneira diferente. O
+        que abordamos aqui nunca deverá substituir uma aula que você teve na
+        faculdade, deve ser para lhe auxiliar a entender um tema que foi
+        introduzido servindo como um auxilio. Sim, incentivamos a formação em
+        nível superior em Tecnologia, embora o mercado de trabalho para
+        tecnologia não se restrinja a profissionais com ensino superior,
+        acreditamos que o ambiente universitário está além de aulas acadêmicas,
+        ele tem o papel do promover conexões que podem gerar uma boa ideia, além
+        do papel na formação social do cidadão.
+      </p>
+      <p>
+        Nosso compromisso é com a didática inclusiva. Buscamos incessantemente
+        aperfeiçoar nossas metodologias de ensino para que o aprendizado seja
+        acessível e eficaz para o maior número de pessoas possível. Estamos
+        buscando ilustrações e recursos visuais que abrangem uma diversidade
+        maior de público, respeitando e celebrando as diferentes formas de
+        aprender. Pra DEVS Experts, ser diferente é normal
+      </p>
+      <p>
+        Se você possui habilidades com ilustração ou acredita ter uma boa
+        didática e deseja contribuir para essa missão, convidamos você a se
+        juntar a nós. Sua arte pode ser a ponte que conecta muitos ao
+        conhecimento, tornando os conceitos mais claros e o aprendizado mais
+        agradável e eficaz.
+      </p>
+      <p></p> O conhecimento não deve ser guardado, se te ajudamos de alguma
+      forma #compartilhe. Acreditamos que o conhecimento deva ser livre e
+      disseminado, permitindo que todos tenham a oportunidade de crescer e se
+      desenvolver. Acreditamos que, ao democratizar o acesso ao conhecimento,
+      estamos contribuindo para um mundo mais justo e equitativo.
+      <p>
+        Apoiamos a técnica de estudo desenvolvida por Richard Feynman, que se
+        baseia em explicar os conceitos de forma simples e clara, como se
+        estivesse ensinando para alguém que não tem conhecimento prévio no
+        assunto. Essa abordagem não apenas facilita a compreensão, mas também
+        solidifica o conhecimento e promove uma aprendizagem mais profunda e
+        significativa.
+      </p>
+      <p>
+        Junte-se a nós nessa missão e vamos, juntos, democratizar o
+        conhecimento. Juntos, podemos fazer a diferença.
+      </p>
     </div>
   );
 }
